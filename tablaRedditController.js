a8fa777b78925a5605d5a82f8b71383c07c9939c
KevinSanz28 <118836162+KevinSanz28@users.noreply.github.com>
2023-02-01 23:12:13 -0600

    Controlador del componente

---
+++ tablaRedditController.js
@@ -0,0 +1,9 @@
+({
+    doInit : function(component, event, helper) {
+        helper.getRedditList(component, event, helper);
+    },
+    
+    delete : function(component, event, helper) {
+    component.find("Reddit");
+}
+ })
